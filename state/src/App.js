e03d9551b14f4de6a3118e12fe6cd59210ceab9d
JoacoMaurtua <joacomaurtua@gmail.com>
2021-06-12 12:08:29 -0500

    proyect

---
+++ state/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Foco from './components/Foco';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Foco/>
+     
+    </div>
+  );
+}
+
+export default App;
