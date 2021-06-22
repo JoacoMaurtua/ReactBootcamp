0402fed51ff206971e17f70a3038da7f2db4eb4a
JoacoMaurtua <joacomaurtua@gmail.com>
2021-06-22 18:40:43 -0500

    learning forms in react

---
+++ forms/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import UseForm from './components/UseForm';
+
+function App() {
+  return (
+    <div className="App">
+      <UseForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ forms/src/components/UseForm.jsx
@@ -0,0 +1,45 @@
+
+import React, { useState } from 'react';
+
+export default function UseForm(props) {
+
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  
+//MOSTARME LOS DATOS DE ENTRADA Y EVTAR RECARGA DE LA PAGINA
+  const onSubmit = (event) =>{
+    event.preventDefault();
+    const data = {userName,email,password};
+    console.log(data);
+    setUserName("");
+    setEmail("");
+    setPassword("");
+  }
+
+  
+  return (
+    <form onSubmit = {onSubmit}>
+      <h1>My first form in React</h1>
+      <br/>
+      <div>
+          <label>Username: </label>
+          <input type="text" value = {userName} onChange = {e=>setUserName(e.target.value)}/>
+      </div>
+      <br/>
+      <div>
+          <label>Email Address: </label>
+          <input type="email" value = {email} onChange = {e=>setEmail(e.target.value)}/>
+      </div>
+      <br/>
+      <div>
+          <label>Password: </label>
+          <input type="password" value = {password} onChange = {e=>setPassword(e.target.value)}/>
+      </div>
+      <br/>
+      <input type="submit" value="Create User"/>{/* SUBMIT ACTIVA EL ONSUBMIT QUE TIENE COMO ATRIBUTO EL FORM */}
+      
+    </form>
+  )
+}
