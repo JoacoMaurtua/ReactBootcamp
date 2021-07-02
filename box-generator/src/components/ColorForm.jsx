95e5ffd51853bc285886142ba2465179a5afe01d
JoacoMaurtua <joacomaurtua@gmail.com>
2021-07-02 11:31:19 -0500

    this is awesome

--- box-generator/src/components/ColorForm.jsx
+++ box-generator/src/components/ColorForm.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState,useRef} from 'react';
 import styled from 'styled-components';
 
 /*
@@ -7,18 +7,30 @@ import styled from 'styled-components';
   console.log(`${nombre} maurtua`)
  */
 
+const StyleForm = styled.form`
+  margin-top: 25px;
+`
 
 const Box = styled.div`
-  background-color: ${props => props.color}; 
-  width: 150px;
-  height: 150px;
+  background-color: ${props => props.color};  //$ llaves es para escribir js dentro de css
+  width: ${props=>props.width};
+  height: ${props=>props.height};
+  display: inline-block;
+  margin-top: 50px;
+  margin-right: 25px;
 `
 
 
 export default function ColorForm() {
 
+  const inputColor = useRef(null);
+  const inputWidth = useRef(null);
+  const inputHeight = useRef(null);
+
   const [state,setState] = useState({
     color:"",
+    width:0,
+    height:0,
     boxes:[]
   });
 
@@ -32,25 +44,62 @@ export default function ColorForm() {
 
   const onSubmit = (event) =>{
     event.preventDefault();
+    const {color,width,height} = state
     setState({
       color:"",
-      boxes:[...state.boxes,{Color:state.color}] //traer las cajitas anteriores y la actual
+      width:0,
+      height:0,
+      boxes:[...state.boxes,{Color:color,
+                            Width:width,
+                            Height:height
+            }] //traer las cajitas anteriores y la actual
     })
+    inputColor.current.value = "";
+    inputWidth.current.value = "";
+    inputHeight.current.value = "";
   }
 
   
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <label htmlFor="color">Color </label>
-        <input type="text" name="color" value={state.color} onChange = {onChange} />
+      <StyleForm onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="color">Color </label>
+          <input type="text" ref={inputColor} name="color" onChange = {onChange} />
+        </div>
+
+        <div>
+          <label htmlFor="width">Width </label>
+          <input type="text" ref={inputWidth} name="width" onChange = {onChange} />
+        </div>
+
+        <div>
+          <label htmlFor="height">Height </label>
+          <input type="text" ref={inputHeight} name="height" onChange = {onChange} />
+        </div>
+
         <input type="submit" value="Add" />
-      </form>
+    
+    
+      </StyleForm>
 
       <div>
-        {state.boxes.map((item,index)=>( <Box key={index} color={item.Color}/>))} 
+        {state.boxes.map((item,index)=>( <Box key={index} color={item.Color} width={item.Width} height={item.Height}/>))} 
       </div>
+
+      {/* background-color: estas recibiendo un props llamado color    color={ }
+      height: estas recibiendo un props llamado height    height={ }
+      width: estas recibiendo un props llamado width    width={ }
+      useState({ boxes: []}) que aqui una vez crees las cajitas , quedaria 
+      asi useState({boxes: [{color: "red", height:100px, width: 100px}, 
+      {color: "green", height:200px, width: 200px}]})
+      
+      
+       boxes:[...state.boxes,{Color:color,
+                            Width:width,
+                            Height:height
+            }] //traer las cajitas anteriores y la actual*/}
       
       
     </div>
