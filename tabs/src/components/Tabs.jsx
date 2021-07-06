657038285f91150c7392e8780b0add78789fb443
JoacoMaurtua <joacomaurtua@gmail.com>
2021-07-06 11:27:54 -0500

    styles

--- tabs/src/components/Tabs.jsx
+++ tabs/src/components/Tabs.jsx
@@ -1,5 +1,29 @@
 import React, { useState } from 'react';
-//import styled from 'styled-components';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  margin-top: 15px;
+  width: 150px;
+  padding: 15px;
+  margin-right: 10px;
+  background-color: #e9e9e9;
+  font-weight: bold;
+
+  :hover{
+    background-color: black;
+    color: #e9e9e9;
+  }
+`
+const DivStyle = styled.div`
+  width: 465px;
+  height: 180px;
+  border: 2px solid #e9e9e9;
+  margin-top: 20px;
+  margin-left: 273px;
+  font-size: 20px;
+  font-weight: bold;
+  padding-top: 50px;
+`
 
 export default function Tabs() {
 
@@ -11,7 +35,7 @@ export default function Tabs() {
     setState({initialTab:value});
   }
 
-  const buttonList = tabs.map((item,index) => {return(<button key={index} onClick={(event) => changeValues(event,item)}>Tab {item}</button>)});
+  const buttonList = tabs.map((item,index) => {return(<Button key={index} onClick={(event) => changeValues(event,item)}>Tab {item}</Button>)});
 
   const {initialTab} = state;
 
@@ -24,10 +48,12 @@ export default function Tabs() {
           {buttonList}
 
         </div>
-        <div className="results">
-          {initialTab}
-        </div>
+        <DivStyle>
+          Tab {initialTab} content here!
+        </DivStyle>
     </div>
   )
 }
+
+
 
