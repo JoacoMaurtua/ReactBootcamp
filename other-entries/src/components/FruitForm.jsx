bfc92e8b784ca5fbca58019d50b0c4033a41de71
JoacoMaurtua <joacomaurtua@gmail.com>
2021-07-07 11:24:57 -0500

    excercise complete

--- other-entries/src/components/FruitForm.jsx
+++ other-entries/src/components/FruitForm.jsx
@@ -10,11 +10,17 @@ const fruits = [
 export default function FruitForm() {
 
   const [state,setState] = useState(fruits[0]);
-  console.log(state);
+  const [tasty,setTasty] = useState(false);
+ 
 
+  const onSubmit = event =>{
+    event.preventDefault();
+    console.log('The ' + state + ' is' + (tasty ? '' : ' not') + ' tasty!');
+  }
 
+  
   return (
-    <form>
+    <form onSubmit = {onSubmit}>
       <select value={state} onChange={event => setState(event.target.value)}>
        {/*  <option value="Banana">Banana</option>
         <option value="Pineapple">Pineapple</option>
@@ -24,14 +30,14 @@ export default function FruitForm() {
           fruits.map((item,index)=>(<option key={index}>{item.replace(/\b\w/g , M => M.toUpperCase())}</option>)) //los regular expressions(REGEX) se utilizan para validaciones 
         }
       </select>
-    
-     {/*  <input type="checkbox"/>Is it tasty? */}
+        
+      <label htmlFor="check" >Is it tasty? </label>
+      <input type="checkbox" id="check" checked={tasty} onChange={event => setTasty(event.target.checked)}/>
      
       <button>Take a bite</button>
 
-
-      
     </form>
   )
 }
+
 
