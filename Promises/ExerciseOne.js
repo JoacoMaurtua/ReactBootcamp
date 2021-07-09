53eaa6598234bc285190c49caea1f35ffea78e34
JoacoMaurtua <joacomaurtua@gmail.com>
2021-07-09 18:19:36 -0500

    whit a asyncronic simulation

--- Promises/ExerciseOne.js
+++ Promises/ExerciseOne.js
@@ -6,7 +6,7 @@ const tossCoin = () =>{
 
 //Cuanto tiempo para que la moneda caiga 5 veces seguidas cara
 
-function fiveHeadsSync(){
+ function fiveHeadsSync(){
   let headsCount = 0;
   let attemps = 0;
   while(headsCount < 5){
@@ -21,7 +21,46 @@ function fiveHeadsSync(){
   }
 
   return `It took ${attemps} tries to flip five "heads"`;
+ 
 }
 
-console.log(fiveHeadsSync());
-console.log("This is run after the fiveHeadsSync function completes");
+//console.log(fiveHeadsSync());
+//console.log("This is run after the fiveHeadsSync function completes"); 
+
+//VERSION UTILIZANDO PROMESAS(SIMULACION ASINCRONA):
+
+function fiveHeadsAsync(){
+  return new Promise((resolve,reject)=>{
+    let headsCount = 0;
+    let attemps = 0;
+    while(headsCount < 5){
+      attemps++;
+      let result = tossCoin(); //cara o sello
+       console.log(`${result} was flipped`);
+       if(result === "heads"){
+         headsCount++;
+       } else{
+         headsCount = 0;
+       }
+    }
+    
+    if(attemps <= 100){
+      resolve(`It took ${attemps} tries to flip five "heads"`);
+    }
+    else{
+      reject(`Take to many attemps`)
+    }
+   
+  });
+}
+
+fiveHeadsAsync()
+    .then(res => console.log(res))
+    .catch(err =>console.log(err));
+    console.log( "When does this run now?" );
+
+
+
+
+
+
