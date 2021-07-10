f9a71d7e6bba1ef93d59e8e460f2a4d0ffc8a89a
JoacoMaurtua <joacomaurtua@gmail.com>
2021-07-10 00:01:50 -0500

    practice useEffect

---
+++ use-efect/src/components/List.jsx
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from 'react';
+
+export default function List() {
+
+  //Nota: useEfect evita la carga de la transferencia de datos hacia la API
+  const [lista,setLista] = useState([]); 
+
+  useEffect(()=>{
+    console.log("fetch veces")
+    fetch('https://swapi.dev/api/people/')
+        .then(data => data.json())
+        .then(data => setLista(data.results)) //trae los datos de la API al array vacio
+        .catch(error => console.error(error)) //codigos de error NOT FOUND 404
+  },[]) //SOLO CARGA UNA VEZ
+
+  //PISTA PARA VER LO DE POKEMON:
+
+  /*const onClick =()=>{
+    fetch(URL)
+  }*/
+  //<button onClick = {onClick}></button>
+ 
+  return (
+    <div>
+      <ul>
+        {lista?lista.map((item,index)=>{return(<li key={index}>{item.name}</li>)}):null}
+      </ul>
+      
+    </div>
+  )
+}
+
