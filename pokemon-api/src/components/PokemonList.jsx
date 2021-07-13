7947e810e4cce5da6e0f72aa37c8b8d2a8d2aaec
JoacoMaurtua <joacomaurtua@gmail.com>
2021-07-12 19:29:00 -0500

    the complete pokemon list

--- pokemon-api/src/components/PokemonList.jsx
+++ pokemon-api/src/components/PokemonList.jsx
@@ -1,20 +1,19 @@
-import React, {useState,useEffect} from 'react'
+import React, {useState} from 'react'
 
 export default function PokemonList() {
 
   const [list,setList] = useState([]);
 
-  useEffect(()=>{
-    console.log("works");
-    fetch("https://pokeapi.co/api/v2/pokemon")
-        .then(pokeData => pokeData.json())
-        .then(pokeData => setList(pokeData.results))
-        .catch(pokeError => alert(pokeError))
-  });
+  const pokeClick = () =>{
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=1118")
+      .then(pokeData => pokeData.json())
+      .then(pokeData => setList(pokeData.results))
+      .catch(pokeError => alert(pokeError))
+  }
 
   return (
     <div>
-      <button>Pokemon fetch</button>
+      <button onClick={pokeClick}>Pokemon fetch</button>
       <ul>
         {list ? list.map((pokemon,index)=>{
           return(<li key={index}>{pokemon.name}</li>)})
