7aedf4bf2a4ea7ebbd38a500cdd4e10892fe8ca5
JoacoMaurtua <joacomaurtua@gmail.com>
2021-07-12 22:20:44 -0500

    learning axios

--- pokemon-api/src/components/PokemonList.jsx
+++ pokemon-api/src/components/PokemonList.jsx
@@ -1,37 +1,50 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import axios from 'axios';
 
 export default function PokemonList() {
 
 
   const [list,setList] = useState([]);
 
-  /* const pokeClick = () =>{
+  /*****FETCH METHOD*****/
+
+   const pokeClick = () =>{
     fetch("https://pokeapi.co/api/v2/pokemon/?limit=1118")
       .then(pokeData => pokeData.json())
       .then(pokeData => setList(pokeData.results))
       .catch(pokeError => alert(pokeError))
-  } */
+  }  
 
-  /*****TRY CATCH WHEY*****/
-  const pokeClick2 = async() =>{
+
+  /*****TRY CATCH METHOD*****/
+    const pokeClick2 = async() =>{ 
     await pokeFetch().then(res => setList(res.results))
   }
 
   const pokeFetch = async() =>{   //declaramos una funcion asincrona
-    try{
-      const pokeData = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=1118");//para que se tome el tiempo necesario para traer la data de la url
-      const pokeJson = await pokeData.json(); //una vez resuelta la promise, conviertelos a JSON(es otra promise)
-      return pokeJson;
-    }catch(err){
-      console.error(err);
-    }
-  }
+      try{
+        const pokeData = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=1118");//para que se tome el tiempo necesario para traer la data de la url
+        const pokeJson = await pokeData.json(); //una vez resuelta la promise, conviertelos a JSON(es otra promise)
+        return pokeJson;
+      }catch(err){
+        console.error(err);
+      }
+  } 
 
+
+  /*****AXIOS METHOD*****/
+  const pokeClick3 = () =>{
+    axios.get("https://pokeapi.co/api/v2/pokemon/?limit=1118")
+
+        .then(response=> setList(response.data.results))
+         
+        .catch(error=> console.error(error))   
+  }
 
 
   return (
     <div>
-      <button onClick={pokeClick2}>Pokemon fetch</button>
+      <button onClick={pokeClick3}>Pokemon fetch</button>
       <ul>
         {list ? list.map((pokemon,index)=>{
           return(<li key={index}>{pokemon.name}</li>)})
@@ -40,6 +53,7 @@ export default function PokemonList() {
     </div>
   )
 }
+
 
 
 
