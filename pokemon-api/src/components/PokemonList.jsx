e911ab36fdab61ff6c619cec7b802c9ecb9cce05
JoacoMaurtua <joacomaurtua@gmail.com>
2021-07-12 20:31:23 -0500

    try catch

--- pokemon-api/src/components/PokemonList.jsx
+++ pokemon-api/src/components/PokemonList.jsx
@@ -2,18 +2,36 @@ import React, {useState} from 'react'
 
 export default function PokemonList() {
 
+
   const [list,setList] = useState([]);
 
-  const pokeClick = () =>{
+  /* const pokeClick = () =>{
     fetch("https://pokeapi.co/api/v2/pokemon/?limit=1118")
       .then(pokeData => pokeData.json())
       .then(pokeData => setList(pokeData.results))
       .catch(pokeError => alert(pokeError))
+  } */
+
+  /*****TRY CATCH WHEY*****/
+  const pokeClick2 = async() =>{
+    await pokeFetch().then(res => setList(res.results))
   }
+
+  const pokeFetch = async() =>{   //declaramos una funcion asincrona
+    try{
+      const pokeData = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=1118");//para que se tome el tiempo necesario para traer la data de la url
+      const pokeJson = await pokeData.json(); //una vez resuelta la promise, conviertelos a JSON(es otra promise)
+      return pokeJson;
+    }catch(err){
+      console.error(err);
+    }
+  }
+
+
 
   return (
     <div>
-      <button onClick={pokeClick}>Pokemon fetch</button>
+      <button onClick={pokeClick2}>Pokemon fetch</button>
       <ul>
         {list ? list.map((pokemon,index)=>{
           return(<li key={index}>{pokemon.name}</li>)})
